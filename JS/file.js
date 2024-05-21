2728f71ace5e8043277a74a9a420f3dbe2502a4d
Gabriel Azevedo <contato@gabesdev.com.br>
2024-05-21 09:10:20 -0300

    Makes the menu sticky

--- JS/file.js
+++ JS/file.js
@@ -1,11 +1,22 @@
-const toggleMenuClass = () => {
-  let menu = document.querySelector(".navbar__list")
-  menu.classList.toggle("menu-open")
-}
-
 const startEvents = () => {
-  let button = document.querySelector(".js-toggle-menu")
-  button.addEventListener("click", toggleMenuClass)
+  let menu = document.querySelector(".navbar__list")
+
+  const _removeMenuOpenClass = () => menu.classList.remove("menu-open")
+  const _toggleMenuOpenClass = () => menu.classList.toggle("menu-open")
+
+  const handleMenu = (e) => {
+    // If we click the menu button, it should toggle
+    if (
+      e.target.classList.contains("js-toggle-menu") ||
+      e.target.parentElement.classList.contains("js-toggle-menu")
+    ) {
+      _toggleMenuOpenClass()
+    } else {
+      _removeMenuOpenClass()
+    }
+  }
+
+  document.addEventListener("click", handleMenu)
 }
 
 window.addEventListener("load", startEvents)
