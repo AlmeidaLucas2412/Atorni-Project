0d529c9952ac723a0f9aeb41f25214d1dde5eceb
Lucas <lucasprepara12@gmail.com>
2024-06-05 11:21:05 -0300

    Improving some SEO things

--- JS/form.js
+++ JS/form.js
@@ -4,6 +4,11 @@ window.onload = function () {
   let modal = document.querySelector('.modal')
   let confirmButton = document.querySelector('.affirmative')
   let denyButton = document.querySelector('.negative')
+  let whatsappButton = document.querySelector('.float-button')
+
+  whatsappButton.addEventListener('click', function () {
+    window.location = "https://api.whatsapp.com/send?phone=55119833814821"
+  })
 
   openButton.addEventListener('click', function () {
     backdrop.style.display = 'block'
